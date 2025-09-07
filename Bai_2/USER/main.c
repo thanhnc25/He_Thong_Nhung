d1c12df288199c30df84e309a3ca57f6ab8e348f
DucAnhVu-15 <vducanh150104@gmail.com>
2025-09-07 13:16:19 +0700

    Bai 2, 2.1m 7/9/2025

--- Bai_2/USER/main.c
+++ Bai_2/USER/main.c
@@ -7,14 +7,14 @@ void LED_Init();
 
 int main()
 {
-	LED_PC_13_Init();
+	LED_Init();
 	
 	while(1){
 		GPIO_SetBits(GPIOC, GPIO_Pin_13);
-		GPIO_SetBits(GPIOB, GPIO_Pin_13);
+		GPIO_SetBits(GPIOB, GPIO_Pin_7);
 		delay_1ms(500);
 		GPIO_ResetBits(GPIOC, GPIO_Pin_13);
-		GPIO_ResetBits(GPIOB, GPIO_Pin_13);
+		GPIO_ResetBits(GPIOB, GPIO_Pin_7);
 		delay_1ms(500);
 	}
 }
@@ -32,7 +32,7 @@ void LED_Init()
 	GPIO_Init(GPIOC, &GPIO);
 	
 	GPIO.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO.GPIO_Pin = GPIO_Pin_9;
+	GPIO.GPIO_Pin = GPIO_Pin_7;
 	GPIO.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOB, &GPIO);
 }
